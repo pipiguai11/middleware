2700a4c5261592969e01baed0d4eb31600e41221
linhw <953522392@qq.com>
2022-01-20 11:09:08 +0800

    feat(redis)：添加redis模块

---
+++ redis/redis-base/src/main/java/com/lhw/redisbase/RedisBaseApplication.java
@@ -0,0 +1,13 @@
+package com.lhw.redisbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisBaseApplication.class, args);
+    }
+
+}
---
+++ redis/redis-base/src/test/java/com/lhw/redisbase/RedisBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhw.redisbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

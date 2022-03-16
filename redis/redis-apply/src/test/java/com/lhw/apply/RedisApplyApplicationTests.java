69d75b6dab29605de1954f620e029a27cc001788
linhw <953522392@qq.com>
2022-03-16 10:40:20 +0800

    feat(redis)：添加redis应用模块

---
+++ redis/redis-apply/src/test/java/com/lhw/apply/RedisApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhw.apply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
